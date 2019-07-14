de3f20551cbe9193a47c05f820846fd623f461d2
blaze <kontaktAK@Yandex.ru>
2019-07-14 15:16:35 +0300

    dev proxy

--- server/express.js
+++ server/express.js
@@ -32,9 +32,9 @@ server.use('/api/users/', users)
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static(path.resolve(__dirname, '../client', 'dist', 'index.html')));
+  server.use(express.static(path.resolve(__dirname, '../client', 'dist', 'index.html')));
 
-  app.get('*', (req, res) => {
+  server.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'dist', 'index.html'));
   });
 }
