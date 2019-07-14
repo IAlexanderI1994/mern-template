7e574a6e8acb1ca454fa82799783ff988e81c0f0
blaze <kontaktAK@Yandex.ru>
2019-07-14 16:35:48 +0300

    dev proxy

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,6 +1,7 @@
 module.exports = {
 
-  mongoURI: 'mongodb://root:root@localhost:27017/mern-template?authSource=admin',
+  //mongoURI: 'mongodb://root:root@localhost:27017/mern-template?authSource=admin',
+  mongoURI: 'mongodb+srv://root:root@cluster0-pad6l.mongodb.net/test?retryWrites=true&w=majority',
   secretOrKey: 'secret'
 }
 
